c3afd2eb8a4f24df209c24028b1866338f1150a2
Andre_Scherping <rageracer13@gmail.com>
2018-06-27 20:59:25 +0200

    GrundProjekt

---
+++ ___4Häuser/4Häuser/Assets/Scripts/GeldProduzieren.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GeldProduzieren : MonoBehaviour {
+
+    public Text GeldText;
+    public Text HolzText;
+    public Text SteinText;
+    public Text MetalText;
+    public Text ÖlText;
+
+    public int Geld;
+    public int Holz;
+    public int Stein;
+    public int Metal;
+    public int Öl;
+    public int zusatzGeld;
+    public int zusatzHolz;
+    public int zusatzStein;
+    public int zusatzMetal;
+    public int zusatzÖl;
+    public int erweiterungenGeld=1;
+    public int erweiterungenHolz = 0;
+    public int erweiterungenStein = 0;
+    public int erweiterungenMetal = 0;
+    public int erweiterungenÖl = 0;
+
+    public Text verbesserungsText;
+
+    private void Start()
+    {
+
+        StartCoroutine(Drucken());
+        StartCoroutine(Fällen());
+        StartCoroutine(Abbauen());
+        StartCoroutine(Schmieden());
+        StartCoroutine(Bohren());
+
+    }
+
+    private void Update()
+    {
+        GeldText.text = Geld.ToString();
+        HolzText.text = Holz.ToString();
+        SteinText.text = Stein.ToString();
+        MetalText.text = Metal.ToString();
+        ÖlText.text = Öl.ToString();
+    }
+
+    public void Zusätze(int stoff)
+    {
+        if (stoff == 0)
+            zusatzGeld++;
+        else if (stoff == 1)
+            zusatzHolz++;
+        else if (stoff == 2)
+            zusatzStein++;
+        else if (stoff == 3)
+            zusatzMetal++;
+        else if (stoff == 4)
+            zusatzÖl++;
+    }
+
+    public void Erweiterungen(int stoff)
+    {
+        if (stoff == 0)
+            erweiterungenGeld++;
+        else if (stoff == 1)
+            erweiterungenHolz++;
+        else if (stoff == 2)
+            erweiterungenStein++;
+        else if (stoff == 3)
+            erweiterungenMetal++;
+        else if (stoff == 4)
+            erweiterungenÖl++;
+    }
+
+    IEnumerator Drucken()
+    {
+        Geld = Geld + zusatzGeld*erweiterungenGeld;
+        yield return new WaitForSeconds(1);
+        StartCoroutine(Drucken());
+    }
+    IEnumerator Fällen()
+    {
+        Holz = Holz + zusatzHolz * erweiterungenHolz;
+        yield return new WaitForSeconds(1);
+        StartCoroutine(Fällen());
+    }
+    IEnumerator Abbauen()
+    {
+        Stein = Stein + zusatzStein * erweiterungenStein;
+        yield return new WaitForSeconds(1);
+        StartCoroutine(Abbauen());
+    }
+    IEnumerator Schmieden()
+    {
+        Metal = Metal + zusatzMetal * erweiterungenMetal;
+        yield return new WaitForSeconds(1);
+        StartCoroutine(Schmieden());
+    }
+    IEnumerator Bohren()
+    {
+        Öl = Öl + zusatzÖl * erweiterungenÖl;
+        yield return new WaitForSeconds(1);
+        StartCoroutine(Bohren());
+    }
+}
---
+++ ___4Häuser/4Häuser/Assets/Scripts/SpielerInventar.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpielerInventar : MonoBehaviour {
+
+    public GameObject player;
+
+    public int spielerGeld;
+    public int spielerHolz;
+    public int spielerStein;
+    public int spielerMetal;
+    public int spielerÖl;
+
+    public Text spielerGeldText;
+    public Text spielerHolzText;
+    public Text spielerSteinText;
+    public Text spielerMetalText;
+    public Text spielerÖlText;
+    public Text AktivierungsText;
+
+    private void Start()
+    {
+        AktivierungsText.text = "";
+    }
+
+    private void Update()
+    {
+         
+        spielerGeldText.text = spielerGeld.ToString();
+        spielerHolzText.text = spielerHolz.ToString();
+        spielerSteinText.text = spielerStein.ToString();
+        spielerMetalText.text = spielerMetal.ToString();
+        spielerÖlText.text = spielerÖl.ToString();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        AktivierungsText.text = "Press E";
+        if (Input.GetKeyDown("e"))
+        {
+
+            if (other.CompareTag("bank"))
+            {
+                spielerGeld = spielerGeld + player.GetComponent<GeldProduzieren>().Geld;
+                player.GetComponent<GeldProduzieren>().Geld = 0;
+            }
+            if (other.CompareTag("holzfäller"))
+            {
+                spielerHolz = spielerHolz + player.GetComponent<GeldProduzieren>().Holz;
+                player.GetComponent<GeldProduzieren>().Holz = 0;
+            }
+            if (other.CompareTag("steinmetz"))
+            {
+                spielerStein = spielerStein + player.GetComponent<GeldProduzieren>().Stein;
+                player.GetComponent<GeldProduzieren>().Stein = 0;
+            }
+            if (other.CompareTag("fabrik"))
+            {
+                spielerMetal = spielerMetal + player.GetComponent<GeldProduzieren>().Metal;
+                player.GetComponent<GeldProduzieren>().Metal = 0;
+            }
+            if (other.CompareTag("labor"))
+            {
+                spielerÖl = spielerÖl + player.GetComponent<GeldProduzieren>().Öl;
+                player.GetComponent<GeldProduzieren>().Öl = 0;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        AktivierungsText.text = "";
+    }
+
+}
