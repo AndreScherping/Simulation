2bf59f937c05821a745cd2a0ac03a753e8f20aa3
Lia <alina.hw@web.de>
2018-07-02 20:17:54 +0200

    finish

---
+++ ___4Häuser/4Häuser/Assets/Scripts/SimpleCarController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor;
+    public bool steering;
+}
+
+public class SimpleCarController : MonoBehaviour
+{
+    public List<AxleInfo> axleInfos;
+    public float maxMotorTorque;
+    public float maxSteerAngle;
+
+    public void ApplyLocalPositionsToVisuals(WheelCollider collider)
+    {
+        if(collider.transform.childCount == 0)
+        {
+            return;
+        }
+        Transform visualWheels = collider.transform.GetChild(0);
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheels.transform.position = position;
+        visualWheels.transform.rotation = rotation;
+    }
+
+	void FixedUpdate ()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteerAngle * Input.GetAxis("Horizontal");
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+            ApplyLocalPositionsToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionsToVisuals(axleInfo.rightWheel);
+        }
+	}
+}
+
